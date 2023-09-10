0e3b068117235afa9f90605ff91e7ca8242b3979
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-09-10 17:54:37 -0300

    Atualizando AJ1.svelte

--- src/AJ1.svelte
+++ src/AJ1.svelte
@@ -8,6 +8,8 @@ import {trocarEstadoDoJogo} from './CEstado'
 </script>
 <body>
       <!--aqui pode colocar alguma coisa sobre--> 
+	  <h2>Percorra todo o labirinto sem perder as 3 vidas ao tocar nas bombas vermelhas,</h2>
+	  <h2>existe um campo de força,onde suas vidas não serão desperdiçadas</h2>
 </body>
 <div id="ajuda" class='menu' on:click="{() => trocarEstadoDoJogo('jogar cruzada 1')}">
 	Fechar
